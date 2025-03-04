79b05de4361897b4582816e69c1d04e914adeb12
rajalishikhe <nahidujjamanraj@gmail.com>
2025-03-04 08:25:53 +0600

    Now I am set and fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-
 import './App.css'
-import Sunglass from './Component/Sunglass/Sunglass'
 import Watch from './Component/Watch/Watch'
 
 
-function App() {
-  
 
+
+function App() {
+  const Watches=[
+    {id:1, name:"Walton",price:"2000"},
+    {id:2, name:"Walton",price:"2000"},
+    {id:3, name:"Walton",price:"2000"},
+    
+  ]
+  
   return (
     <>
       
-      <h1> React Part Start</h1>
-      <Watch></Watch>
-      <Sunglass></Sunglass>
+      <h1> React Part Start1</h1>
+      {
+        Watches.map(watch=><Watch key={watch.id} item={watch}></Watch>)
+
+      }
       
-      
-    </>
+      </>
   )
 }
 
--- src/Component/Watch/Watch.jsx
+++ src/Component/Watch/Watch.jsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import { add, multiplication } from '../../Utility/calculate';
 import './Watch.css';
 
-const Watch = () => {
-    const first_value=12;
-    const second_value=33;
-    const multiplication_two_number=multiplication(first_value,second_value)
-    const add_two_number=add(first_value,second_value);
-
-    return (
+const Watch = ({item}) => {
+    const {name,price}=item
+ return (
         <div className='border_name'>
-            <h1>Watch section</h1>
-            <p>Total watch{multiplication_two_number}</p>
-            <p>Add number{add_two_number}</p>
-            
-        </div>
+            <h1>Watches</h1>
+            <p>{name}</p>
+            <p>{price}</p>
+         </div>
     );
 };
 
